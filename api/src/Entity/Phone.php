3d14649db9ed02813fcb7c83301861c8719f1b46
StarbugStone <pint2stone@gmail.com>
2019-08-11 10:29:58 +0200

    Adding the phone images entity

--- api/src/Entity/Phone.php
+++ api/src/Entity/Phone.php
@@ -115,9 +115,15 @@ class Phone
      */
     private $phoneHasFeatures;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\PhoneImage", mappedBy="phone", orphanRemoval=true)
+     */
+    private $phoneImages;
+
     public function __construct()
     {
         $this->phoneHasFeatures = new ArrayCollection();
+        $this->phoneImages = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -198,6 +204,37 @@ class Phone
             // set the owning side to null (unless already changed)
             if ($phoneHasFeature->getPhone() === $this) {
                 $phoneHasFeature->setPhone(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|PhoneImage[]
+     */
+    public function getPhoneImages(): Collection
+    {
+        return $this->phoneImages;
+    }
+
+    public function addPhoneImage(PhoneImage $phoneImage): self
+    {
+        if (!$this->phoneImages->contains($phoneImage)) {
+            $this->phoneImages[] = $phoneImage;
+            $phoneImage->setPhone($this);
+        }
+
+        return $this;
+    }
+
+    public function removePhoneImage(PhoneImage $phoneImage): self
+    {
+        if ($this->phoneImages->contains($phoneImage)) {
+            $this->phoneImages->removeElement($phoneImage);
+            // set the owning side to null (unless already changed)
+            if ($phoneImage->getPhone() === $this) {
+                $phoneImage->setPhone(null);
             }
         }
 
---
+++ api/src/Entity/PhoneImage.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\PhoneImageRepository")
+ */
+class PhoneImage
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+
+    /**
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Phone", inversedBy="phoneImages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getPhone(): ?Phone
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?Phone $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function setImageFile(File $imageFile = null)
+    {
+        $this->imageFile = $imageFile;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($imageFile) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile(): File
+    {
+        return $this->imageFile;
+    }
+}
