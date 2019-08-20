9eb597ae604bbf063662389afa1166ee65fe0123
StarbugStone <pint2stone@gmail.com>
2019-08-20 18:03:33 +0200

    Adding the subresource so client gets all his users in the client interface

--- api/src/Entity/Client.php
+++ api/src/Entity/Client.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -39,6 +40,7 @@ class Client implements UserInterface
 
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\ClientUser", mappedBy="client")
+     * @ApiSubresource
      */
     private $clientUsers;
 
