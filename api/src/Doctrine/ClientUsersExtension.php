4a646f648f86340c7c80db287fb3dff945592207
StarbugStone <pint2stone@gmail.com>
2019-08-20 21:51:19 +0200

    Adding a Doctrine Extension to allow a Get on clientUsers and only return the clients users, unless we are admin

---
+++ api/src/Doctrine/ClientUsersExtension.php
@@ -0,0 +1,47 @@
+<?php
+//api\src\Doctrine\ClientUsersExtension.php
+
+namespace App\Doctrine;
+
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\ClientUser;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
+
+class ClientUsersExtension implements QueryCollectionExtensionInterface
+{
+
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function applyToCollection(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        string $operationName = null
+    ) {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        $client = $this->security->getUser();
+
+        if ($resourceClass !== ClientUser::class || $this->security->isGranted('ROLE_ADMIN') || $client === null) {
+            return;
+
+        }
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf(':current_client MEMBER OF %s.client', $rootAlias));
+        $queryBuilder->setParameter('current_client', $client);
+    }
+}
