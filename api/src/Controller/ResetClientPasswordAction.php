1665a79a71506f42ed30f03816a82c8d5ba2a10b
StarbugStone <pint2stone@gmail.com>
2019-09-19 14:49:04 +0200

    looking into the reset user. Bit of brainstorming and UML to get all things sorted

--- api/src/Controller/ResetClientPasswordAction.php
+++ api/src/Controller/ResetClientPasswordAction.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Controller;
 
 
@@ -38,7 +37,18 @@ class ResetClientPasswordAction
     public function resetPassword(string $email)
     {
         dd($email);
+
+        //TODO: check if email is in base, DO NOT SEND ERROR IF NOT
+
+        //TODO: add token to client
+
+        //TODO: send email to client with new token for reset
+
+
         // TODO: Return a properly formatted json response
+
+        //TODO: No, we can use the validation groups and verify all here. The path is just /users/ID/reset. All the rest is passed via json
+        //TODO: make sure we are all open. This will be used for the new password and the reset password.
     }
 
 }
