f008991f5b2ebafa13b5836c6b4a6f0c2243e098
StarbugStone <pint2stone@gmail.com>
2019-11-24 10:43:00 +0100

    Looking into the cleint activation tests

--- api/tests/functional/createUserTest.php
+++ api/tests/functional/createUserTest.php
@@ -5,14 +5,28 @@ namespace App\Tests\functional;
 
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Client;
 use App\Tests\userAuthTrait;
 use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class createUserTest extends ApiTestCase
 {
     use RefreshDatabaseTrait;
     use userAuthTrait;
+
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    protected function setUp():void
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
 
     public function testUserCreationAdmin()
     {
@@ -29,20 +43,25 @@ class createUserTest extends ApiTestCase
         $obj = json_decode($response->getContent());
         $idUrl = $obj->{"@id"};
 
-        $response = $client->request('GET', $idUrl,[
-            'json'=>[]
+        $response = $client->request('GET', $idUrl, [
+            'json' => []
         ]);
         $this->assertResponseIsSuccessful();
 
         $obj = json_decode($response->getContent());
 
-        $this->assertEquals('test1',$obj->username);
-        $this->assertEquals('pass1@dev.com',$obj->email);
-        $this->assertContains('ROLE_CLIENT',$obj->roles);
+        $this->assertEquals('test1', $obj->username);
+        $this->assertEquals('pass1@dev.com', $obj->email);
+        $this->assertContains('ROLE_CLIENT', $obj->roles);
         $this->assertFalse(in_array('ROLE_ADMIN', $obj->roles));
         $this->assertFalse($obj->active);
 
-        
+        //Get the created user from the database. Needed to get the token to activate user
+        /** @var Client $apiClient */
+        $apiClient = $this->entityManager->getRepository(Client::class)->findOneBy(array('email' => $obj->email));
+
+        $apiClientToken = $apiClient->getNewUserToken();
+//        dd($apiClient);
 
     }
 
