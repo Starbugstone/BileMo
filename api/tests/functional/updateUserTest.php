834520688c9ceac3eb5609f4391fc2115c42fa42
StarbugStone <pint2stone@gmail.com>
2019-11-25 14:01:00 +0100

    continuing the user tests

--- api/tests/functional/updateUserTest.php
+++ api/tests/functional/updateUserTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\functional;
 
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Client;
 use App\Tests\userAuthTrait;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
@@ -13,19 +14,38 @@ class updateUserTest extends ApiTestCase
     use ReloadDatabaseTrait;
     use userAuthTrait;
 
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    protected function setUp():void
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
     public function testUpdateClient1()
     {
         $client = $this->authUser('client1', 'password');
 
-        //get our ID
-        $response = $client->request('PUT', '/clients', [
+        //make sure we are logged in properly
+        $client->request('GET', '/clients', [
             'json' => []
         ]);
-
         $this->assertResponseIsSuccessful();
 
-        $obj = json_decode($response->getContent());
-        $idUrl = $obj->{"@id"};
+        /** @var Client $apiClient */
+        $apiClient = $this->entityManager->getRepository(Client::class)->findOneBy(array('username' => 'client1'));
+
+        $apiClientId = $apiClient->getId();
+
+//        $obj = json_decode($response->getContent());
+//        $idUrl = $obj->{"@id"};
+//        dd($obj);
 
 
 
